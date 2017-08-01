aa8a7d26d7e8aec293567e9c80b19a3d065dbaa6
邵铁军 <shaotiejun@syswin.com>
2017-08-01 10:46:57 +0800

    增加多用户功能

--- services/articleServ.js
+++ services/articleServ.js
@@ -19,7 +19,7 @@ const findById = async (_id) => {
  */
 const findByUserId = async (userId, {page=0, limit=20}) => {
   try {
-    const res = await ArticleModel.find({user_id: userId}).skip(page * limit).limit(limit).exec()
+    const res = await ArticleModel.find({user_id: userId}).populate('user_id', 'name head phone').skip(page * limit).limit(limit).exec()
     return res
   } catch (e) {
     throw e
@@ -31,7 +31,7 @@ const findByUserId = async (userId, {page=0, limit=20}) => {
  */
 const findAll = async ({page=0, limit=20}) => {
   try {
-    const res = await ArticleModel.find().populate('user_id user', 'name head phone').skip(page * limit).limit(limit).sort({'_id': -1}).exec()
+    const res = await ArticleModel.find().populate('user_id', 'name head phone').skip(page * limit).limit(limit).sort({'_id': -1}).exec()
     return res
   } catch (e) {
     throw e
